387b0718a9092fc35a86bcd89e953ae50ab90ae1
robinpunn <robin.punnoose@protonmail.com>
2026-06-08 18:37:54 -0500

    db script updates to handle stat corrections

--- nfl/nflverse/scripts/utils/db.py
+++ nfl/nflverse/scripts/utils/db.py
@@ -9,12 +9,21 @@ def get_connection():
 
 def insert_rows(cur, table, df, conflict_cols):
     cols = df.columns
-    placeholders = ", ".join(["%s"] * len(cols))
+    
+    placeholders = ", ".join(["%s"] * len(cols)) 
     col_names = ", ".join(cols)
+   
+    conflict_list = [c.strip() for c in conflict_cols.split(",")]
+
+    update_cols = [c for c in cols if c not in conflict_list]
+
+    update_assignments = ", ".join([f"{c} = EXCLUDED.{c}" for c in update_cols])
+
     query = f"""
         INSERT INTO nflverse.{table} ({col_names})
         VALUES ({placeholders})
-        ON CONFLICT ({conflict_cols}) DO NOTHING
+        ON CONFLICT ({conflict_cols}) 
+        DO UPDATE SET {update_assignments}
     """
     for row in df.iter_rows(named=True):
         cur.execute(query, tuple(row.values()))
