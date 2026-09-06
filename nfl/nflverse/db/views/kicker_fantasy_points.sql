b61d51217ff5a8c59575c84669547521d2daadb5
robinpunn <robin.punnoose@protonmail.com>
2026-09-06 11:24:36 -0500

    aded nflverse views to calculate points for players and kickers

---
+++ nfl/nflverse/db/views/kicker_fantasy_points.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE VIEW nflverse.kicker_fantasy_weekly AS
+SELECT
+    player_id,
+    name,
+    team,
+    season,
+    week,
+    season_type,
+
+    COALESCE(made_list, ARRAY[]::integer[]) AS made_list,
+
+    COALESCE(made, 0) AS field_goals_made,
+
+    COALESCE(
+        (
+            SELECT SUM(distance)
+            FROM unnest(COALESCE(made_list, ARRAY[]::integer[])) AS distance
+        ),
+        0
+    ) AS field_goal_yards,
+
+    COALESCE(pat_made, 0) AS extra_points_made,
+
+    ROUND(
+        (
+            COALESCE(
+                (
+                    SELECT SUM(distance)
+                    FROM unnest(COALESCE(made_list, ARRAY[]::integer[])) AS distance
+                ),
+                0
+            ) / 10.0
+            + COALESCE(pat_made, 0)
+        ),
+        2
+    ) AS fantasy_points
+
+FROM nflverse.player_kicking;
